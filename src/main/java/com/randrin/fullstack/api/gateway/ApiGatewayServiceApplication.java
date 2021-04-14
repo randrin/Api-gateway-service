01f9baeab33dba094a06dacd1a0295069e1738a2
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2021-04-14 16:10:33 +0200

    Enable Hystrix

--- src/main/java/com/randrin/fullstack/api/gateway/ApiGatewayServiceApplication.java
+++ src/main/java/com/randrin/fullstack/api/gateway/ApiGatewayServiceApplication.java
@@ -3,9 +3,11 @@ package com.randrin.fullstack.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
